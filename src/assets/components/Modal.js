978be893e624759dd79fd6381856f69d2045f435
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-20 16:11:44 -0700

    Add properties to Dialog and DialogTitle.

--- src/assets/components/Modal.js
+++ src/assets/components/Modal.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Dialog, DialogTitle } from '@material-ui/core/';
 import { ModalFooter, ModalForm } from './';
 
 function Table() {
   return (
-    <div>
+    <Dialog open={true}>
+      <DialogTitle id="form-dialog-title">Add User</DialogTitle>
       <ModalFooter />
       <ModalForm />
-    </div>
+    </Dialog>
   );
 }
 
