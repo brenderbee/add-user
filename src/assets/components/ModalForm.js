8bcbe6374bf361846a883734eee6a7dcfe46b0cb
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-20 16:22:41 -0700

    Import DialogContent and TextField from material-ui. Add first name, last name, and email fields.

--- src/assets/components/ModalForm.js
+++ src/assets/components/ModalForm.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { DialogContent, TextField } from '@material-ui/core/';
 
 function ModalForm() {
   return (
-    <div>
-      ModalForm
-    </div>
+    <DialogContent>
+      <TextField
+        autoFocus
+        margin="normal"
+        id="last-name"
+        label="Last Name"
+        type="text"
+        fullWidth
+        required
+      />
+      <TextField
+        autoFocus
+        margin="normal"
+        id="first-name"
+        label="First Name"
+        type="text"
+        fullWidth
+        required
+      />
+      <TextField
+        autoFocus
+        margin="normal"
+        id="name"
+        label="Email Address"
+        type="email"
+        fullWidth
+        required
+      />
+    </DialogContent>
   );
 }
 
