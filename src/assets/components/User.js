030a0c0fce91162715b6253645a071d77c84a09a
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-20 11:16:04 -0700

    Import React Fragment and material-ui ListItem, ListItemText, and Dividers components.

--- src/assets/components/User.js
+++ src/assets/components/User.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { ListItem, ListItemText, Divider } from '@material-ui/core';
 import { UserActions, UserImage } from './';
 
 function User() {
   return (
-    <div>
-      <UserImage />
-      <UserActions />
-    </div>
+    <Fragment>
+      <ListItem>
+        <UserImage />
+        <ListItemText
+          primary="Last, First"
+          secondary="test@gmail.com"
+        />
+        <UserActions />
+      </ListItem>
+      <Divider />
+    </Fragment>
   );
 }
 
