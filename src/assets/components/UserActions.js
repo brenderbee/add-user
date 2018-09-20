37e73e11c84b3eb2bada7468a8f386b498d5fcfc
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-20 15:32:41 -0700

    Import Button component and make a delete and edit button.

--- src/assets/components/UserActions.js
+++ src/assets/components/UserActions.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 
 function UserActions() {
   return (
     <div>
-      UserActions
+      <Button variant="outlined" color="primary">
+        Delete
+        <DeleteIcon />
+      </Button>
+      <Button variant="outlined" color="secondary">
+        Edit
+        <EditIcon />
+      </Button>
     </div>
   );
 }
