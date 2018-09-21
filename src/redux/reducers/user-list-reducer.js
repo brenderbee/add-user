c711ce916079428396be18600d428ab0514830d5
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-21 14:05:18 -0700

    Connect to firebase. May be wrong place to connect.

--- src/redux/reducers/user-list-reducer.js
+++ src/redux/reducers/user-list-reducer.js
@@ -1,9 +1,14 @@
 import constants from './../constants';
+import firebase from 'firebase';
+import constants from './../constants';
 
+const { firebaseConfig } = constants;
 const { c } = constants;
 
+firebase.initializeApp(firebaseConfig);
+
 const initialState ={
-  masterUserList: {}
+  masterUserList: firebase.database().ref('users').once('value').then((snapshot)=> snapshot.val())
 }
 
 function userListReducer(state = initialState, action) {
