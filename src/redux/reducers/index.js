41706f8a11ae33709b3d7664a81a3562bdcaaf82
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-21 14:04:18 -0700

    Import modalDisplayReducer and add to rootReducer.

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import userListReducer from './ticket-list-reducer';
+import modalDisplayReducer from './modal-display-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-  masterUserList: userListReducer
+  masterUserList: userListReducer,
+  modalDisplay: modalDisplayReducer
 });
 
 export default rootReducer;
