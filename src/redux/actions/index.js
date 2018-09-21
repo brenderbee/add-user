a8777a755399a0dcd167b4351648c43545cb5a11
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-21 13:57:00 -0700

    Import ActionTypes. Create openModal and closeModal action creators.

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,21 @@
 import firebase from 'firebase';
 import constants from './../constants';
+import * as types from "./../constants/ActionTypes";
 
 const { firebaseConfig } = constants;
 
 firebase.initializeApp(firebaseConfig);
+
+export function openModal() {
+  return {
+    type: types.OPEN_MODAL,
+    payload: true
+  }
+}
+
+export function closeModal() {
+  return {
+    type: types.CLOSE_MODAL,
+    payload: false
+  }
+}
