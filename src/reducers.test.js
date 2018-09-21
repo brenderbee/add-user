8d9ab521bdfd268ac1d1159ab5735fc80c4537d9
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-21 14:45:38 -0700

    Rewrite tests for modalDisplayReducer.

--- src/reducers.test.js
+++ src/reducers.test.js
@@ -4,22 +4,44 @@ import modalDisplayReducer from './redux/reducers/modal-display-reducer';
 import rootReducer from './redux/reducers/';
 
 describe('Add User App', () => {
-  const { initialState } = constants;
-  // const store = createStore(rootReducer, initialState);
+  const initialStateModal = {
+    modalDisplay: false
+  };
 
-  const modalState = { modalDisplay: false };
+  const initialStateUserList = {
+    masterUserList: {
+      random: {
+        id: '0',
+        first_name: 'Fenneko',
+        last_name: 'Fox',
+        email: 'monotone@example.com'
+      },
+      random2: {
+        id: '1',
+        first_name: 'Restuko',
+        last_name: 'Panda',
+        email: 'aggrestuko@example.com'
+      },
+      random3: {
+        id: '2',
+        first_name: 'Ton',
+        last_name: 'Pig',
+        email: 'bossman@example.com'
+      }
+    }
+  };
 
   describe('modalDisplayReducer', () => {
-    it('Should accept and return initial state.', () => {
-      expect(modalDisplayReducer(modalState, { type: null })).toEqual(modalState);
+    it('Should accept and return modalDisplay initial state.', () => {
+      expect(modalDisplayReducer(initialStateModal, { type: null })).toEqual(initialStateModal);
     });
 
-    it('Should change intial state to true.', () => {
-      expect(modalDisplayReducer(modalState, { type: 'OPEN_MODAL', payload: true })).toEqual({ modalDisplay: true });
+    it('Should change intial state of modalDisplay to true.', () => {
+      expect(modalDisplayReducer(initialStateModal, { type: 'OPEN_MODAL', payload: true })).toEqual({ modalDisplay: true });
     });
 
-    it('Should change intial state to false.', () => {
-      expect(modalDisplayReducer(modalState, { type: 'CLOSE_MODAL', payload: false })).toEqual({ modalDisplay: false });
+    it('Should change state of modalDisplay to false.', () => {
+      expect(modalDisplayReducer(initialStateModal, { type: 'CLOSE_MODAL', payload: false })).toEqual({ modalDisplay: false });
     });
 
   });
@@ -28,10 +50,10 @@ describe('Add User App', () => {
   //   it('Should accept and return initial state.', () => {
   //     expect(rootReducer(initialState, { type: null })).toEqual(initialState);
   //   });
-  //
-  //   it('Should contain logic from both reducers.', () => {
-  //     expect(store.getState().modalDisplay.toEqual(modalDisplayReducer(undefined, { type: null }));
-  //   });
+
+    // it('Should contain logic from both reducers.', () => {
+    //   expect(store.getState().modalDisplay.toEqual(modalDisplayReducer(undefined, { type: null }));
+    // });
   // });
 
 });
