ce078abd360275be7c14ab3a24fd4dcce3d5cd02
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-22 14:41:40 -0700

    Connect component to store and map state to props. Create handleOpenAddModal to execute sendOpenModal function so that openModal action creater is dispatched.

--- src/assets/components/AddUser.js
+++ src/assets/components/AddUser.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Tooltip, Button } from '@material-ui/core/';
 import AddIcon from '@material-ui/icons/Add';
+import Proptypes from 'prop-types';
+import { connect } from 'react-redux';
+import { openModal } from './../../redux/actions';
 
-function AddUser() {
+function AddUser(props) {
+  const handleOpenAddModal = () => {
+    props.modalDisplay();
+  }
+
   return (
     <div>
       <Tooltip title="Add">
         <Button
+          onClick={handleOpenAddModal}
           variant="fab"
           color="secondary"
           aria-label="Add"
@@ -19,4 +27,16 @@ function AddUser() {
   );
 }
 
-export default AddUser;
+AddUser.proptypes = {
+  modalDisplay: Proptypes.bool
+}
+
+const mapStateToProps = state => ({
+  modalDisplay: AddUser(state.modalDisplay)
+});
+
+const mapDispatchToProps = dispatch => ({
+  modalDisplay: () => dispatch(openModal())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddUser);
