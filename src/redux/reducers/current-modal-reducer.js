90d28f6935234a91c42b73a45b0a67f479b815c7
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-22 23:05:47 -0700

    Add case for CLOSE_MODAL.

--- src/redux/reducers/current-modal-reducer.js
+++ src/redux/reducers/current-modal-reducer.js
@@ -5,6 +5,8 @@ function currentModalReducer(state = {}, action) {
   switch(action.type) {
     case c.ADD_USER:
       return 'ADD_USER';
+    case c.CLOSE_MODAL:
+      return 'null';
     default:
       return state;
   }
