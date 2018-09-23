deddead5563d9b4536e8b520aaff6e49caace775
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-22 22:49:56 -0700

    Add ADD_USER, EDIT_USER, and NEW_USER. Remove OPEN_MODAL and CLOSE_MODAL.

--- src/redux/constants/actionTypes.js
+++ src/redux/constants/actionTypes.js
@@ -1,3 +1,3 @@
 export const NEW_USER = 'NEW_USER';
-export const OPEN_MODAL = 'OPEN_MODAL';
-export const CLOSE_MODAL = 'CLOSE_MODAL';
+export const ADD_USER = 'ADD_USER';
+export const EDIT_USER = 'EDIT_USER';
