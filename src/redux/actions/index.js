f5b413aa60920a8cbf500e60b46cfd728dc890ee
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-23 09:32:29 -0700

    Remove payload from openAddModal, openEditModal, and closeModal action creators (payload not required).

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -8,21 +8,18 @@ firebase.initializeApp(firebaseConfig);
 
 export function openAddModal() {
   return {
-    type: types.ADD_USER,
-    payload: 'ADD_USER'
+    type: types.ADD_USER
   }
 }
 
 export function openEditModal() {
   return {
-    type: types.EDIT_USER,
-    payload: 'EDIT_USER'
+    type: types.EDIT_USER
   }
 }
 
 export function closeModal() {
   return {
-    type: types.CLOSE_MODAL,
-    payload: ''
+    type: types.CLOSE_MODAL
   }
 }
