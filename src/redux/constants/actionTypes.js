194dcc13b88c22377131002392201922cc0f14e4
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-23 22:21:34 -0700

    Add FETCH_USERS to action types.

--- src/redux/constants/actionTypes.js
+++ src/redux/constants/actionTypes.js
@@ -2,3 +2,4 @@ export const NEW_USER = 'NEW_USER';
 export const ADD_USER = 'ADD_USER';
 export const EDIT_USER = 'EDIT_USER';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
+export const FETCH_USERS = 'FETCH_USERS';
