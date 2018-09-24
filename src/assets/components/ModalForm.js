4bb2591ecd13cc92cddb28eb74ba5290daeea35f
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-24 00:42:12 -0700

    Connect to store. Add closeModal and addUser actions. Create handleFormSubmit().

--- src/assets/components/ModalForm.js
+++ src/assets/components/ModalForm.js
@@ -1,40 +1,50 @@
 import React from 'react';
 import { DialogContent, TextField } from '@material-ui/core/';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { ModalFooter } from './';
+import { closeModal, addUser } from './../../redux/actions';
 
 function ModalForm(props) {
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    props.sendNewUser({email: 'hello'});
+    props.sendCloseModal();
+  }
+
   return (
     <DialogContent>
-      <TextField
-        autoFocus
-        margin="normal"
-        id="first-name"
-        label="First Name"
-        type="text"
-        value={props.firstName}
-        fullWidth
-        required
+      <form onSubmit={handleFormSubmit}>
+        <TextField
+          autoFocus
+          margin="normal"
+          id="first-name"
+          label="First Name"
+          type="text"
+          value={props.firstName}
+          fullWidth
         />
-      <TextField
-        autoFocus
-        margin="normal"
-        id="last-name"
-        label="Last Name"
-        type="text"
-        value={props.lastName}
-        fullWidth
-        required
-      />
-      <TextField
-        autoFocus
-        margin="normal"
-        id="name"
-        label="Email Address"
-        type="email"
-        value={props.email}
-        fullWidth
-        required
-      />
+        <TextField
+          autoFocus
+          margin="normal"
+          id="last-name"
+          label="Last Name"
+          type="text"
+          value={props.lastName}
+          fullWidth
+        />
+        <TextField
+          autoFocus
+          margin="normal"
+          id="name"
+          label="Email Address"
+          type="email"
+          value={props.email}
+          fullWidth
+        />
+        <ModalFooter onFormSubmit={handleFormSubmit}/>
+      </form>
     </DialogContent>
   );
 }
@@ -45,4 +55,14 @@ ModalForm.propTypes = {
   email: PropTypes.string
 };
 
-export default ModalForm;
+const mapStateToProps = state => ({
+  currentModal: state.currentModal,
+  masterUserList: state.masterUserList
+});
+
+const mapDispatchToProps = dispatch => ({
+  sendCloseModal: () => dispatch(closeModal()),
+  sendNewUser: (newUser) => dispatch(addUser(newUser))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ModalForm);
