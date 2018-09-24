39a8d31852540d1a397a03c6bc486162c43421ab
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-23 20:36:22 -0700

    Remove firebase from reducer.

--- src/redux/reducers/user-list-reducer.js
+++ src/redux/reducers/user-list-reducer.js
@@ -1,13 +1,5 @@
 import constants from './../constants';
-// const { c } = constants;
-
-// import firebase from 'firebase';
-// const { firebaseConfig } = constants;
-
-// firebase.initializeApp(firebaseConfig);
-// const initialState ={
-//   masterUserList: firebase.database().ref('users').once('value').then((snapshot)=> snapshot.val())
-// }
+const { c } = constants;
 
 function userListReducer(state = {}, action) {
   switch(action.type) {
