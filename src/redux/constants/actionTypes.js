f7dd0822983d41bdf6ad88492f3d96b33bcca035
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-24 01:10:49 -0700

    Add DELETE_USER action type.

--- src/redux/constants/actionTypes.js
+++ src/redux/constants/actionTypes.js
@@ -1,5 +1,7 @@
-export const NEW_USER = 'NEW_USER';
 export const ADD_USER = 'ADD_USER';
+export const DELETE_USER = 'DELETE_USER';
 export const EDIT_USER = 'EDIT_USER';
-export const CLOSE_MODAL = 'CLOSE_MODAL';
 export const FETCH_USERS = 'FETCH_USERS';
+export const NEW_USER = 'NEW_USER';
+
+export const CLOSE_MODAL = 'CLOSE_MODAL';
