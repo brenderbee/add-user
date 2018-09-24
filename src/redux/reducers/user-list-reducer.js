1d02a539f65c1ea8eae762eeee0f09557416f9b2
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-23 22:23:48 -0700

    Add FETCH_USERS case to userListReducer.

--- src/redux/reducers/user-list-reducer.js
+++ src/redux/reducers/user-list-reducer.js
@@ -3,6 +3,8 @@ const { c } = constants;
 
 function userListReducer(state = {}, action) {
   switch(action.type) {
+    case c.FETCH_USERS:
+      return action.payload;
     default:
       return state;
   }
