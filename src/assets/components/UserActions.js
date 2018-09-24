1495c6d3be039587ab3b2454aa788b8aba072597
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-24 09:36:59 -0700

    Add handleDeleteUser method to execute sendDeleteUser. Add Proptypes.

--- src/assets/components/UserActions.js
+++ src/assets/components/UserActions.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Proptypes from 'prop-types';
 import { Button } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import './../css/UserActions.css';
-import { openEditModal } from './../../redux/actions';
+import { openEditModal, deleteUser } from './../../redux/actions';
 
 function UserActions(props) {
 
   const handleOpenEditModal = () => {
     props.sendOpenEditModal();
   }
-  
+
+  const handleDeleteUser = () => {
+    props.sendDeleteUser(props.currentUser.id);
+  }
+
   return (
     <div>
       <Button
+        onClick={handleDeleteUser}
         className="user-button"
         variant="outlined"
         color="primary"
@@ -37,12 +43,20 @@ function UserActions(props) {
   );
 }
 
+UserActions.proptypes = {
+  currentModal: Proptypes.string,
+  masterUserList: Proptypes.object,
+  currentUser: Proptypes.object
+}
+
 const mapStateToProps = state => ({
-  currentModal: UserActions(state.currentModal)
+  currentModal: state.currentModal,
+  masterUserList: state.masterUserList
 });
 
 const mapDispatchToProps = dispatch => ({
-  sendOpenEditModal: () => dispatch(openEditModal())
+  sendOpenEditModal: () => dispatch(openEditModal()),
+  sendDeleteUser: (userId) => dispatch(deleteUser(userId))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserActions);
