a826a01db9fc6820c01094a25d6d9323e6eeeb91
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-24 01:13:00 -0700

    Add DELETE_USER case to userListReducer.

--- src/redux/reducers/user-list-reducer.js
+++ src/redux/reducers/user-list-reducer.js
@@ -3,6 +3,8 @@ const { c } = constants;
 
 function userListReducer(state = {}, action) {
   switch(action.type) {
+    case c.DELETE_USER:
+      return action.payload;
     case c.NEW_USER:
       return action.payload
     case c.FETCH_USERS:
