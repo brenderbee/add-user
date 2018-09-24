72e27bd98ab6fc9cd72f9d8cd6fbc3272004b459
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-23 22:19:41 -0700

    Remove references to jsonResponse and firebase.

--- src/index.js
+++ src/index.js
@@ -3,19 +3,8 @@ import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from "redux-thunk";
 import { Provider } from 'react-redux';
-import firebase from 'firebase';
 import App from './App';
 import rootReducer from './redux/reducers';
-// import { jsonResponse } from './tempData';
-import constants from './redux/constants';
-
-// const { c } = constants;
-
-const { firebaseConfig } = constants;
-
-firebase.initializeApp(firebaseConfig);
-
-// const usersRef = firebase.database().ref('users').once('value').then((snapshot)=> snapshot.val());
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 const store = createStore(
