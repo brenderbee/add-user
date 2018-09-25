cf2b5c2b5210c11be355e7b575259ab89dab079d
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-24 21:53:22 -0700

    Update App test so that it matches the setup in index.js.

--- src/App.test.js
+++ src/App.test.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import rootReducer from './redux/reducers';
-import { createStore } from 'redux';
+import reduxThunk from "redux-thunk";
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
-const store = createStore(rootReducer, {
-  currentModal: null
-}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(
+  rootReducer,
+  {
+    currentModal: '',
+    masterUserList: {},
+    currentUser: {}
+  },
+  applyMiddleware(reduxThunk)
+);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
