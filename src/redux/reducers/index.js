e9602b56a692165e185e3110346462caa939a985
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-24 19:42:14 -0700

    Import currentUserReducer and set it to  currentUser.

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import userListReducer from './user-list-reducer';
 import currentModalReducer from './current-modal-reducer';
+import currentUserReducer from './current-user-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   masterUserList: userListReducer,
-  currentModal: currentModalReducer
+  currentModal: currentModalReducer,
+  currentUser: currentUserReducer
 });
 
 export default rootReducer;
