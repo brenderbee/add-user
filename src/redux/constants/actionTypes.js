71c24646fd905a702b3d8178bc67f0ba54042877
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-24 20:21:28 -0700

    Add RESET_USER action type.

--- src/redux/constants/actionTypes.js
+++ src/redux/constants/actionTypes.js
@@ -3,6 +3,7 @@ export const DELETE_USER = 'DELETE_USER';
 export const EDIT_USER = 'EDIT_USER';
 export const FETCH_USERS = 'FETCH_USERS';
 export const NEW_USER = 'NEW_USER';
+export const RESET_USER = 'RESET_USER';
 export const SET_USER = 'SET_USER';
 
 export const CLOSE_MODAL = 'CLOSE_MODAL';
