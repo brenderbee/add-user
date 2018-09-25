512d54ef5a8d1bd7cd40dca707d5e50f70f2cca7
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-24 20:26:07 -0700

    Add RESET_USER case to currentUserReducer.

--- src/redux/reducers/current-user-reducer.js
+++ src/redux/reducers/current-user-reducer.js
@@ -3,6 +3,8 @@ const { c } = constants;
 
 function currentUserReducer(state = {}, action) {
   switch(action.type) {
+    case c.RESET_USER:
+      return action.payload;
     case c.SET_USER:
       return action.payload;
     default:
