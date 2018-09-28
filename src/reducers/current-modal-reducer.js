265e0eb1123d18e1149b0108e0490f82fe1e257a
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-28 12:47:02 -0700

    Change modal action constants from _USER to _MODAL.

--- src/reducers/current-modal-reducer.js
+++ src/reducers/current-modal-reducer.js
@@ -3,10 +3,10 @@ const { c } = constants;
 
 function currentModalReducer(state = {}, action) {
   switch(action.type) {
-    case c.ADD_USER:
-      return 'ADD_USER';
-    case c.EDIT_USER:
-      return 'EDIT_USER';
+    case c.ADD_MODAL:
+      return 'ADD_MODAL';
+    case c.EDIT_MODAL:
+      return 'EDIT_MODAL';
     case c.CLOSE_MODAL:
       return '';
     default:
