339e646747bb5fc373cf47600fec06ff2b91cf80
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-27 23:37:27 -0700

    Create Main component and import Table and ModalConductor.

---
+++ src/assets/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import { Table, ModalConductor } from './';
+
+function Main() {
+  return (
+    <Fragment>
+      <Table />
+      <ModalConductor />
+    </Fragment>
+  );
+}
+
+export default Main;
