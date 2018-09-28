af0c050ac84185f60da78a738440f1a00c9f9f9c
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-28 00:11:24 -0700

    Add basic login form.

--- src/assets/components/Login.js
+++ src/assets/components/Login.js
@@ -1,29 +1,30 @@
-import React, { Fragment } from 'react';
-import { FormControl, TextField } from '@material-ui/core/';
+import React from 'react';
+import { TextField, Typography } from '@material-ui/core/';
+import './../css/Login.css';
 
 function Login() {
   return (
-    <Fragment>
-      <FormControl>
-        <TextField
-          autoFocus
-          margin="normal"
-          id="login-email"
-          label="Email"
-          type="text"
-          fullWidth
-          required
-        />
-        <TextField
-          margin="normal"
-          id="login-password"
-          label="password"
-          type="text"
-          fullWidth
-          required
-        />
-      </FormControl>
-    </Fragment>
+    <div className="login">
+      <Typography variant="headline">Login</Typography>
+      <Typography variant="caption">enter credentials to access app</Typography>
+      <TextField
+        autoFocus
+        margin="normal"
+        id="login-email"
+        label="Email"
+        type="text"
+        fullWidth
+        required
+      />
+      <TextField
+        margin="normal"
+        id="login-password"
+        label="password"
+        type="text"
+        fullWidth
+        required
+      />
+    </div>
   );
 }
 
