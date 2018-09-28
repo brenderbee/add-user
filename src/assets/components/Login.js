9243a7b1eac8c6eda36052906d3b58e3e7c4c8d0
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-28 00:53:02 -0700

    Add heading and Button.

--- src/assets/components/Login.js
+++ src/assets/components/Login.js
@@ -1,29 +1,38 @@
 import React from 'react';
-import { TextField, Typography } from '@material-ui/core/';
+import { Button, TextField, Typography } from '@material-ui/core/';
 import './../css/Login.css';
 
 function Login() {
   return (
     <div className="login">
-      <Typography variant="headline">Login</Typography>
-      <Typography variant="caption">enter credentials to access app</Typography>
-      <TextField
-        autoFocus
-        margin="normal"
-        id="login-email"
-        label="Email"
-        type="text"
-        fullWidth
-        required
-      />
-      <TextField
-        margin="normal"
-        id="login-password"
-        label="password"
-        type="text"
-        fullWidth
-        required
-      />
+      <form>
+        <Typography variant="headline">Login</Typography>
+        <Typography variant="caption">enter credentials to access app</Typography>
+        <TextField
+          autoFocus
+          margin="normal"
+          id="login-email"
+          label="Email"
+          type="text"
+          fullWidth
+          required
+        />
+        <TextField
+          margin="normal"
+          id="login-password"
+          label="password"
+          type="text"
+          fullWidth
+          required
+        />
+        <Button
+          variant="outlined"
+          color="primary"
+          size="small"
+        >
+          Login
+        </Button>
+      </form>
     </div>
   );
 }
