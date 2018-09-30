dba08b377f3a62bb7f1ecbe820b87340f333b5fd
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-29 22:30:06 -0700

    Create login-email-reducer.

---
+++ src/reducers/login-email-reducer.js
@@ -0,0 +1,13 @@
+import constants from './../constants';
+const { c } = constants;
+
+function loginEmailReducer(state = '', action) {
+  switch(action.type) {
+    case c.EMAIL_AUTH:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+export default currentModalReducer;
