eca30d6707d7758a2ad4b1a1cf3c645e7274e191
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-30 11:08:13 -0700

    Remove loginError state for now.

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ const store = createStore(
     currentUser: {},
     loginEmail: '',
     loginPassword: '',
-    loginError: '',
     loading: false
   },
   composeEnhancers(applyMiddleware(reduxThunk))
