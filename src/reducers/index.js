6cfa494f204dcec603bf1a10b908ed7b9faf3b86
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-29 22:40:09 -0700

    Import loginEmailReducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import userListReducer from './user-list-reducer';
 import currentModalReducer from './current-modal-reducer';
 import currentUserReducer from './current-user-reducer';
+import loginEmailReducer from './login-email-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   masterUserList: userListReducer,
   currentModal: currentModalReducer,
-  currentUser: currentUserReducer
+  currentUser: currentUserReducer,
+  loginEmail: loginEmailReducer
 });
 
 export default rootReducer;
