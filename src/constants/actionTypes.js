eb23d0ad7263db1d1d1e4dc74c7b6ad300a3a06a
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-09-29 22:29:42 -0700

    Add authentication constants.

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -3,8 +3,17 @@ export const ADD_MODAL = 'ADD_MODAL';
 export const EDIT_MODAL = 'EDIT_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
 
+// Interacting with firebase data
 export const DELETE_USER = 'DELETE_USER';
 export const FETCH_USERS = 'FETCH_USERS';
 export const NEW_USER = 'NEW_USER';
 export const RESET_USER = 'RESET_USER';
 export const SET_USER = 'SET_USER';
+
+// Authentication
+export const EMAIL_AUTH = 'EMAIL_AUTH';
+export const EMAIL_LOGOUT = 'EMAIL_LOGOUT';
+export const PASSWORD_AUTH = 'PASSWORD_AUTH';
+export const PASSWORD_LOGOUT = 'PASSWORD_LOGOUT';
+export const LOADING = 'LOADING';
+export const LOADING_COMPLETE = 'LOADING_COMPLETE';
